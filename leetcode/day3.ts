657ebfa9c4497cbf639a7d65c840d63ac8667bba
Chadie Gil <caugis@gmail.com>
2024-12-04 11:36:42 +0800

    day-3 remove duplicates from sorted array

---
+++ leetcode/day3.ts
@@ -0,0 +1,32 @@
+/**
+ * Remove Duplicates from Sorted Array
+ *  Example 1:
+    Input: nums = [1,1,2]
+    Output: 2, nums = [1,2,_]
+    Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
+    It does not matter what you leave beyond the returned k (hence they are underscores).
+    Example 2:
+
+    Input: nums = [0,0,1,1,1,2,2,3,3,4]
+    Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+    Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
+    It does not matter what you leave beyond the returned k (hence they are underscores).
+ */
+
+const numsDuplicate = [1, 1, 1, 2, 2, 2, 4, 5]
+
+const removeDuplicates = (nums: number[]): number => {
+  let counter = 0
+  const temp: number[] = []
+  nums.forEach((ele: number) => {
+    if (!temp.includes(ele)) {
+      temp.push(ele)
+      nums[counter] = ele
+      counter++
+    }
+  })
+  return counter
+}
+
+const resultForDuplicates = removeDuplicates(numsDuplicate)
+console.log("resultForDuplicates", resultForDuplicates)
